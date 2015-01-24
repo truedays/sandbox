d94b1ae926397e8d539ee1f7890f6ce2af6dce21
truedays <ray@truedays.org>
2015-01-24 12:15:27 -0600

    now taking CLI arguments

--- python/cta.py
+++ python/cta.py
@@ -1,18 +1,30 @@
+#!/usr/bin/python
+# ray@truedays.org
+# CTA API CLI YAH
+
 import sys
 import requests
 from bs4 import BeautifulSoup
 f = open('./.cta-api.key', 'r')
-apikey = "?key=" + f.read(25)
+APIKEY = "?key=" + f.read(25)
 f.close()
 
+URL="http://www.ctabustracker.com/bustime/api/v1/"
+
 if len(sys.argv) <2:
- apicmd = "getpredictions"
- apiargv = "&rt=20&stpid=456"
+# apicmd = "getpredictions"
+# apiargv = "&rt=20&stpid=456"
+ apicmd = "gettime"
+ apiargv = ""
+else:
+ apicmd = sys.argv[1]
+ print "apicmd: " + apicmd
+ apiargv = sys.argv[2]
+ print "apiargv: " + apiargv
 
 
-
-print 'http://www.ctabustracker.com/bustime/api/v1/' + apicmd + apikey + apiargv
-r = requests.get('http://www.ctabustracker.com/bustime/api/v1/' + apicmd + apikey + apiargv)
+print URL + apicmd + APIKEY + apiargv
+r = requests.get(URL + apicmd + APIKEY + apiargv)
 
 
 
@@ -23,4 +35,4 @@ print r.text
 #print soup
 
 
-print apikey
+#print APIKEY
